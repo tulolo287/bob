cd04fd2b7fb25bd8f42e6c454ab587db33a63ae8
Tolo <iv1055@yandex.ru>
2022-04-03 19:58:45 +0000

    init bob

--- android/src/com/bob/AndroidLauncher.java
+++ android/src/com/bob/AndroidLauncher.java
@@ -10,11 +10,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-<<<<<<< HEAD
-		config.useCompass = false;
-		//config.useWakelock = true;
-=======
->>>>>>> 518773d (init bob)
 		initialize(new Start(), config);
 	}
 }
